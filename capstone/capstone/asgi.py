a6d23a2e665fa89080c3c4cc57a4f08d5d7fd7ee
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-01-07 17:07:43 -0300

    Configuration stuff

--- capstone/capstone/asgi.py
+++ capstone/capstone/asgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import controlqueue.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+	'http': get_asgi_application(),
+	'websocket': AuthMiddlewareStack(
+		URLRouter(controlqueue.routing.websocket_urlpatterns)
+	)
+})
