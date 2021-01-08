376db9e0d3493382b7fca003762a20fb5cbbfa8d
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-01-08 11:36:20 -0300

    Simple tests for index view'

--- capstone/mysite/tests/test_views.py
+++ capstone/mysite/tests/test_views.py
@@ -8,15 +8,42 @@ APP_NAME = MysiteConfig.name
 
 from controlqueue.consumers import QueueConsumer
 
+
 class Index(TestCase):
-	def setUp(self):
-		self.my_admin_data = {
+	@classmethod
+	def setUpTestData(cls):
+		cls.my_admin_data = {
 			'username': 'test_user',
 			'password': '12345'
 		}
-		self.my_admin = User(username=self.my_admin_data['username'], is_staff=True, is_superuser=True)
-		self.my_admin.set_password(self.my_admin_data['password'])
-		self.my_admin.save()
+		cls.my_admin = User(username=cls.my_admin_data['username'], is_staff=True, is_superuser=True)
+		cls.my_admin.set_password(cls.my_admin_data['password'])
+		cls.my_admin.save()
+
+	def test_get_as_anonymous_user(self):
+		response = self.client.get(reverse(f'{APP_NAME}:index'))
+		self.assertEqual(response.status_code, 200)
+	
+	def test_get_as_superuser(self):
+		self.client.login(
+			username=self.my_admin_data['username'],
+			password=self.my_admin_data['password']
+		)
+
+		response = self.client.get(reverse(f'{APP_NAME}:index'))
+		self.assertEqual(response.status_code, 200)
+
+
+class WaitingQueue(TestCase):
+	@classmethod
+	def setUpTestData(cls):
+		cls.my_admin_data = {
+			'username': 'test_user',
+			'password': '12345'
+		}
+		cls.my_admin = User(username=cls.my_admin_data['username'], is_staff=True, is_superuser=True)
+		cls.my_admin.set_password(cls.my_admin_data['password'])
+		cls.my_admin.save()
 
 	def test_get_as_anonymous_user(self):
 		response = self.client.get(reverse(f'{APP_NAME}:waiting_queue'))
