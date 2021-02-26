74a3b39b5f292e69da6c6401f573761bcbed3e35
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-02-26 15:41:52 -0300

    Base to start working on the navigation

---
+++ capstone/mysite/static/mysite/JavaScript/indexPage/servicesSection.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const buttonPrevious = document.querySelector('#navigationBtnLeft');
+	const buttonNext = document.querySelector('#navigationBtnRight');
+	
+	console.log(buttonNext);
+	console.log(buttonPrevious);
+});
