29ba28e6b631efe4b1cad2ced9f76699f52fd51d
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2021-03-06 11:26:38 -0300

    base for the service slider

--- capstone/mysite/static/mysite/JavaScript/indexPage/servicesSection.js
+++ capstone/mysite/static/mysite/JavaScript/indexPage/servicesSection.js
@@ -1,7 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-	const buttonPrevious = document.querySelector('#navigationBtnLeft');
-	const buttonNext = document.querySelector('#navigationBtnRight');
+class Slider {
+	buttonPrevious = document.querySelector('#navigationBtnLeft');
+	buttonNext = document.querySelector('#navigationBtnRight');
 	
-	console.log(buttonNext);
-	console.log(buttonPrevious);
+	slides = document.querySelector('#slides');
+	currentSlide = 0; // Start on the first slide
+	maxLengthOfSlides = 4;
+
+	constructor() {
+		this.buttonNext.onclick = this.handleGoToNextSlide;
+		this.buttonPrevious.onclick = this.handleGoToPreviousSlide;
+	}
+	
+	handleGoToNextSlide = () => {
+		if (this.currentSlide < this.maxLengthOfSlides) {
+			this.currentSlide++;
+			this.moveToSlide(this.currentSlide);
+		}
+	};
+
+	handleGoToPreviousSlide = () => {
+		if (this.currentSlide > 0) {
+			this.currentSlide--;
+			this.moveToSlide(this.currentSlide);
+		}
+	};
+
+	moveToSlide(slideNum) {
+		const currentMarginLeft = -(slideNum * 100);
+		this.slides.style.marginLeft = `${currentMarginLeft}%`;
+	}
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	const slider = new Slider();
 });
