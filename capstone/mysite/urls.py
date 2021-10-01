f506c7e7b8c0c9c3cac00e5f1383a833e8cf483c
matheus costa <matheuscosta.tech@gmail.com>
2021-10-01 11:38:18 -0300

    New url to submit new post

--- capstone/mysite/urls.py
+++ capstone/mysite/urls.py
@@ -9,9 +9,15 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('waitingQueue/', views.waiting_queue, name='waiting_queue'),
     path('bookingForm/', views.booking_form, name='booking_form'),
-    
     # ----------------------------- API URLs --------------------------------
-    path('more-user-feedback/<int:page_number>/',
-         views.get_more_user_feedbacks,
-         name='more_user_feedback'),
+    path(
+        'more-user-feedback/<int:page_number>/',
+        views.get_more_user_feedbacks,
+        name='more_user_feedback',
+    ),
+    path(
+        'submit-new-feedback/',
+        views.submit_new_feedback,
+        name='submit_new_user_feedback',
+    ),
 ]
