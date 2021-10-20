d4deff0ac6d997529f17eb8a0c594f6974227658
matheus costa <matheuscosta.tech@gmail.com>
2021-10-20 10:58:28 -0300

    Fix mobile menu and booking buttons

--- capstone/mysite/static/mysite/JavaScript/indexPage/hamburgerMenu.js
+++ capstone/mysite/static/mysite/JavaScript/indexPage/hamburgerMenu.js
@@ -1,44 +1,57 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const body = document.querySelector('body');
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("body");
 
-	const btnHamburger = body.querySelector('#btnHamburger');
-	const mobileMenu = body.querySelector('#menuForMobile');
-	const overlay = body.querySelector('#overlay');
+  const btnHamburger = body.querySelector("#btnHamburger");
+  const mobileMenu = body.querySelector("#menuForMobile");
+  const menuItems = body.querySelectorAll(".mobile-menu__item");
+  const overlay = body.querySelector("#overlay");
 
-	btnHamburger.addEventListener('click', function () {
-		if (mobileMenu.classList.contains('mobile-menu--open'))
-			handleCloseMenu();
-		else
-			handleOpenMenu();
-	});
+  btnHamburger.addEventListener("click", function () {
+    if (mobileMenu.classList.contains("mobile-menu--open")) handleCloseMenu();
+    else handleOpenMenu();
+  });
 
-	function handleCloseMenu() {
-		mobileMenu.classList.remove('mobile-menu--open');
-		btnHamburger.classList.remove('navbar__toggle--open');
-		body.classList.remove('noScroll');
+  for (let item of menuItems) {
+    item.addEventListener("click", () => {
+      body.classList.remove("noScroll");
+      overlay.style.display = "none";
+      mobileMenu.classList.remove("mobile-menu--open");
+      btnHamburger.classList.remove("navbar__toggle--open");
 
-		overlay.classList.remove('fade-in');
-		overlay.classList.add('fade-out');
-		overlay.style.display = 'none';
+      for (const element of mobileMenu.children) {
+        element.classList.remove("fade-in");
+        element.classList.add("fade-out");
+      }
+    });
+  }
 
-		for (const element of mobileMenu.children) {
-			element.classList.remove('fade-in');
-			element.classList.add('fade-out');
-		}
-	}
+  function handleCloseMenu() {
+    mobileMenu.classList.remove("mobile-menu--open");
+    btnHamburger.classList.remove("navbar__toggle--open");
+    body.classList.remove("noScroll");
 
-	function handleOpenMenu() {
-		mobileMenu.classList.add('mobile-menu--open');
-		btnHamburger.classList.add('navbar__toggle--open');
-		body.classList.add('noScroll');
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+    overlay.style.display = "none";
 
-		overlay.classList.remove('fade-out');
-		overlay.style.display = 'block';
-		overlay.classList.add('fade-in');
+    for (const element of mobileMenu.children) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    }
+  }
 
-		for (const element of mobileMenu.children) {
-			element.classList.remove('fade-out');
-			element.classList.add('fade-in');
-		}
-	}
+  function handleOpenMenu() {
+    mobileMenu.classList.add("mobile-menu--open");
+    btnHamburger.classList.add("navbar__toggle--open");
+    body.classList.add("noScroll");
+
+    overlay.classList.remove("fade-out");
+    overlay.style.display = "block";
+    overlay.classList.add("fade-in");
+
+    for (const element of mobileMenu.children) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    }
+  }
 });
