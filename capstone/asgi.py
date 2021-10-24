bff9d310c27f8cac8905a73a5b1f7c15f81b99a4
matheus costa <matheuscosta.tech@gmail.com>
2021-10-24 06:03:24 -0300

    Further configuration for deployment

--- capstone/asgi.py
+++ capstone/asgi.py
@@ -1,24 +1,24 @@
-"""
-ASGI config for capstone project.
+# --------------------- Has to come before anything else ---------------------
+from django.core.asgi import get_asgi_application
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
+get_asgi_app = get_asgi_application()
+# ----------------------------------------------------------------------------
 
 import os
+import controlqueue.routing
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import controlqueue.routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.settings')
 
-application = ProtocolTypeRouter({
-	'http': get_asgi_application(),
-	'websocket': AuthMiddlewareStack(
-		URLRouter(controlqueue.routing.websocket_urlpatterns)
-	)
-})
+
+application = ProtocolTypeRouter(
+    {
+        'http': get_asgi_app,
+        'websocket': AuthMiddlewareStack(
+            URLRouter(controlqueue.routing.websocket_urlpatterns)
+        ),
+    }
+)
--- capstone/urls.py
+++ capstone/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('mysite.urls')),
-    path('restrict/', include('controlqueue.urls'))
+    path('admin/', admin.site.urls),
+    path('restrict/', include('controlqueue.urls')),
 ]
